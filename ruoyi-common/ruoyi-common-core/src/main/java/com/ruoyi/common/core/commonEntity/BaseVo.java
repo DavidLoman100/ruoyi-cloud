dde4177035c9fefba54befa344d9a6f61abf90e5
DavidLoman100 <375042957@qq.com>
2025-03-29 17:40:20 +0800

    新增vo基础类

---
+++ ruoyi-common/ruoyi-common-core/src/main/java/com/ruoyi/common/core/commonEntity/BaseVo.java
@@ -0,0 +1,25 @@
+package com.ruoyi.common.core.commonEntity;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author DavidLoman
+ * @create 2025-03-29 12:11
+ */
+@Data
+@Schema(description = "基础返回对象")
+public class BaseVo {
+    @Schema(description = "创建者")
+    private String createBy;
+    @Schema(description = "创建时间")
+    private Date createTime;
+    @Schema(description = "更新者")
+    private String updateBy;
+    @Schema(description = "更新时间")
+    private Date updateTime;
+    @Schema(description = "删除标志 (0代表存在 1代表删除)")
+    private String isDeleted;
+}
